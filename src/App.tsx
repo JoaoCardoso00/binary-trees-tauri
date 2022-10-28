19e9ba1335504235c133ddba6c3f69db0a6ec29a
JoaoCardoso00 <jvcq13@gmail.com>
2022-10-28 18:37:58 -0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,36 @@
+import { BinarySearchTree, useBinarySearchTree } from "react-tree-vis";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+export default function App() {
+  const { ref, insert, remove } = useBinarySearchTree();
+  const [insertValue, setInsertValue] = useState(0);
+  const [removeValue, setRemoveValue] = useState(0);
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="MainContainer">
+      <div>
+        <input
+          type="number"
+          onChange={(elem) =>
+            setInsertValue(parseInt(elem.currentTarget.value, 10))
+          }
+        />
+        <button onClick={() => insert(insertValue)}>Insert</button>
+        <br />
+        <input
+          type="number"
+          onChange={(elem) =>
+            setRemoveValue(parseInt(elem.currentTarget.value, 10))
+          }
+        />
+        <button onClick={() => remove(removeValue)}>Remove</button>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-      <p>{greetMsg}</p>
+      <BinarySearchTree
+        data={[2, 1, 3]}
+        ref={ref}
+        treeStyles={{ nodeShadow: "0 0 1px 1px #0000000" }}
+      />
     </div>
   );
 }
-
-export default App;
