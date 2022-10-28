6fa7759770fd4e843c9b4463064a1f2bbadbfae4
JoaoCardoso00 <jvcq13@gmail.com>
2022-10-28 18:42:03 -0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,32 @@ export default function App() {
 
   return (
     <div className="MainContainer">
-      <div>
-        <input
-          type="number"
-          onChange={(elem) =>
-            setInsertValue(parseInt(elem.currentTarget.value, 10))
-          }
-        />
-        <button onClick={() => insert(insertValue)}>Insert</button>
-        <br />
-        <input
-          type="number"
-          onChange={(elem) =>
-            setRemoveValue(parseInt(elem.currentTarget.value, 10))
-          }
-        />
-        <button onClick={() => remove(removeValue)}>Remove</button>
-      </div>
       <BinarySearchTree
         data={[2, 1, 3]}
         ref={ref}
         treeStyles={{ nodeShadow: "0 0 1px 1px #0000000" }}
       />
+      <div className="inputcontainer">
+        <div className="container">
+          <input
+            type="number"
+            onChange={(elem) =>
+              setInsertValue(parseInt(elem.currentTarget.value, 10))
+            }
+          />
+          <button onClick={() => insert(insertValue)}>Insert</button>
+        </div>
+
+        <div className="container">
+          <input
+            type="number"
+            onChange={(elem) =>
+              setRemoveValue(parseInt(elem.currentTarget.value, 10))
+            }
+          />
+          <button onClick={() => remove(removeValue)}>Remove</button>
+        </div>
+      </div>
     </div>
   );
 }
